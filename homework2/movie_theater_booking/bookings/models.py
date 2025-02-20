a90274f1d69307422c5f0e7a030df3d12bf8a1cb
James <jhpipboy@gmail.com>
2025-02-20 17:11:25 +0000

    Started viewsets for CRUD operations

--- homework2/movie_theater_booking/bookings/models.py
+++ homework2/movie_theater_booking/bookings/models.py
@@ -2,9 +2,19 @@ from django.db import models
 
 # Create your models here.
 class Movie(models.Model):
-    title = models.Charfield(max_length = 255)
+    title = models.CharField(max_length = 255)
     descrition = models.TextField()
     release_date = models.DateField()
-    duration = models.PositiveIntegerField(help="Duration of movie in minutes")
+    duration = models.PositiveIntegerField(help_text="Duration of movie in minutes")
 
-class Seat
+
+class Seat(models.Model):
+    seat_number = models.CharField(max_length=3, unique=True)
+    booking_status = models.BooleanField(default=False)
+
+
+class Booking(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    seat = models.ForeignKey(Seat, on_delete = models.CASCADE)
+    user = models.CharField(max_length=225)
+    booking_date = models.DateField()
--- homework2/movie_theater_booking/bookings/views.py
+++ homework2/movie_theater_booking/bookings/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+
 
 # Create your views here.
+class MovieViewSet(viewsets.ViewSet):
+    """
+    ViewSet for listing and retrieving movies.
+    """
+
+class SeatViewSet(viewsets.ViewSet):
+    """
+    ViewSet for listing and retrieving seats.
+    """
+
+class BookingViewSet(viewsets.ViewSet):
+    """
+    ViewSet for listing and retrieving booking history.
+    """
